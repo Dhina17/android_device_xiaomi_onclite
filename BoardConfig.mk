9f7b571bfd38e88796a512b8afd3f74ae46cf398
Etan Cohen <etancohen@google.com>
2017-03-09 09:37:48 -0800

    onclite: Enable Wi-Fi Aware feature support.
    Enable the Wi-Fi Aware feature on:
    - Add feature to framework
    - Device HAL feature
    
    Bug: 36088628
    Test: integration (non-data-path) sl4a tests pass
    Change-Id: I3e2dca6477b27b466e2b074c70595813fe365973
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,6 +225,7 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_HIDL_FEATURE_AWARE := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/onclite/BoardConfigVendor.mk
