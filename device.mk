1f0a304ab769d5b4c2c4403bdb0e0a687d4fc8bb
Dhina17 <dhinalogu@gmail.com>
2019-10-18 02:39:45 +0000

    onclite: Build configstore 1.1 HAL
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -116,7 +116,7 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service
+    android.hardware.configstore@1.1-service
 
 # Display
 PRODUCT_PACKAGES += \
