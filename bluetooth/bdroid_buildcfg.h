0e78d8b1c138d007d2f3fe904e433e392497cb2a
Dhina17 <dhinalogu@gmail.com>
2019-11-02 14:54:27 +0000

    onclite: bluetooth: Set BTM_DEF_LOCAL_NAME
    In this commit 30ca2b61d39d0a58628e7d57766b9ecc39fbe324 ,BTM_DEF_LOCAL_NAME was nuked because of common-ifying the mido tree
    I just get back this for onclite....
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -16,6 +16,9 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME   "Redmi 7"
+
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS    7
