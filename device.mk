e17b46a4704e7f26b63564b4cf4fcfbef86acb86
Dhina17 <dhinalogu@gmail.com>
2019-11-19 11:28:54 +0000

    onclite: AEX-ify
    - lineage mk files --> aosp mk files
    - Nuke some lineage-stuffs
    - Move some device specific overlays from overlay-lineage to overlay
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -18,7 +18,6 @@ $(call inherit-product, vendor/xiaomi/onclite/onclite-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
@@ -334,10 +333,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
