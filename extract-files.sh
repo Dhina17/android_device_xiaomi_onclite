18d4a08bcbb3362c46472df57fbd654635593013
Dhina17 <dhinalogu@gmail.com>
2019-12-14 14:35:59 +0100

    onclite: extractor-scripts: Update vendor path for aosp
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -25,9 +25,9 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+AOSP_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$AOSP_ROOT"/vendor/aosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -56,12 +56,12 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
+setup_vendor "$DEVICE" "$VENDOR" "$AOSP_ROOT" true "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 # Load camera.msm8953.so shim
-CAM_MSM8953="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/hw/camera.msm8953.so
+CAM_MSM8953="$AOSP_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/hw/camera.msm8953.so
 patchelf --add-needed camera.msm8953_shim.so "$CAM_MSM8953"
 
 "$MY_DIR"/setup-makefiles.sh
