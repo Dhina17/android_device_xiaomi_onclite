b2fd1ba5296f9a1b9208ef2c233f018a7488601b
Dhina17 <dhinalogu@gmail.com>
2020-01-27 20:09:41 +0100

    onclite: Uprev vendor.display.config to 1.11
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -145,7 +145,7 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libgenlock \
     libtinyxml \
-    vendor.display.config@1.1
+    vendor.display.config@1.11
 
 # Doze mode
 PRODUCT_PACKAGES += \
